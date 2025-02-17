40c25c7f43ca88931ddc3aad78223f8ab025ec44
pdkang <pdkang@yahoo.com>
2025-02-16 22:16:53 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,4 @@
 # streamlit_app.py
 import streamlit as st
-import os
 
-port = int(os.environ.get("PORT", 8080))
-st.write(f"Hello, World! Port: {port}")  # Include the port in the output
-
-if __name__ == "__main__":
-    st.run(server_address="0.0.0.0", server_port=port)
+st.write("Hello, World!")
